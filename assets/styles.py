6df962189f23a280b36c9aa4f83864890a7046e5
Eric Rusli <82727843+3L1NK@users.noreply.github.com>
2024-08-25 20:30:47 +0200

    Add files via upload

--- assets/styles.py
+++ assets/styles.py
@@ -1,102 +1,119 @@
+# Style settings for the navigation bar
 NAVBAR_STYLE = {
-  "height" : "80px",
-  'paddingLeft': '10px',
-  'marginBottom' : '20px'
+  'height': '80px',  # Set the height of the navbar to 80 pixels
+  'paddingLeft': '10px',  # Add padding to the left side of the navbar
+  'marginBottom': '20px'  # Add margin at the bottom to separate it from other content
 }
 
-LAYOUT_STYLE = {
-  "paddingLeft": "0px",
-  "paddingRight": "0px",
-}
-
-INPUT_STYLE = {
-  'width': '120px',
-  'fontWeight': 'bold',
-}
-
-STOCK_INFO_CONTAINER_STYLE = {
-  'backgroundColor': '#F5F5F5',
-  'borderRadius': '20px',
-  'padding': '20px',
-  'width': '100%',
-}
-
-GTREND_CONTAINER_STYLE = {
-  'backgroundColor': '#F5F5F5',
-  'borderRadius': '20px',
-  'padding': '20px',
-  'paddingBottom': '20px',
-  'width': '100%',
-}
-
-COMPANY_NAME_STYLE = {
-  'color': '#444444',
-  'fontSize': '24px',
-  'marginTop': '20px',
-  'marginLeft': '10px',
-  'fontWeight': 'bold'
-}
-
-TICKER_STYLE = {
-  'letterSpacing': '1px',
-  'marginLeft': '10px',
-  'width': '100px'
-}
-
-STOCK_PRICE_STYLE = {
-  'color': '#444444',
-  'marginLeft': '10px',
-  'marginTop': '10px',
-  'width': '100px',
-  'text-align': 'right'
-}
-
-CURRENCY_STYLE = {
-  'color': '#444444', # #7F7F7F
-  'fontSize': '20px',
-  'marginTop': '20px',
-  # 'marginLeft': '0px',
-  # 'marginRight': '0px',
-  'margin-right': '0px',
-  'padding-left': '8px',
-  'fontWeight': 'bold',
-  'text-align': 'right'
-}
-
-INDICATOR_CONTAINER_STYLE = {
-  'backgroundColor': '#F5F5F5',
-  'borderRadius': '20px',
-  'padding': '20px',
-  'marginTop': '10px'
-}
-
-INDICATOR_TERM_STYLE = {
-  'color': '#444444',  # #7F7F7F
-  'fontWeight': 'bold'
-}
-
-INDICATOR_STYLE = {
-  'color': '#444444',  # #7F7F7F
-}
-
-PREDICTION_PLOT_CONTAINER_STYLE = {
-  'maxWidth': '120%',
-  'backgroundColor': '#F5F5F5',
-  'marginBottom': '20px',
-  'borderRadius': '20px',
-  'padding': '20px',
-  'marginTop': '20px'
-}
-
+# Style settings for the titles of the components (e.g. 'Google Trends')
 TITLE_STYLE = {
-  'marginLeft': '10px',
+  'marginLeft': '10px',  # Add margin to the left of the title
 }
 
+# Style settings for the overall layout
+LAYOUT_STYLE = {
+  'paddingLeft': '0px',  # Remove padding on the left side of the layout
+  'paddingRight': '0px',  # Remove padding on the right side of the layout
+}
+
+# Style settings for the stock ticker dropdown
+INPUT_STYLE = {
+  'width': '120px',  # Set a fixed width for the dropdown
+  'fontWeight': 'bold',  # Make the text in the dropdown bold
+}
+
+# Style settings for the stock information container
+STOCK_INFO_CONTAINER_STYLE = {
+  'backgroundColor': '#F5F5F5',  # Set a light gray background color
+  'borderRadius': '20px',  # Make the corners of the container rounded
+  'padding': '20px',  # Add padding inside the container
+  'width': '100%',  # Make the container take up the full width of its parent
+}
+
+# Style settings for the company name (stock information container)
+COMPANY_NAME_STYLE = {
+  'color': '#444444',  # Set the text color to dark gray
+  'fontSize': '24px',  # Set the font size to 24 pixels
+  'marginTop': '20px',  # Add margin to the top
+  'marginLeft': '10px',  # Add margin to the left
+  'fontWeight': 'bold'  # Make the text bold
+}
+
+# Style settings for the stock ticker (e.g. AAPL) (stock information container)
+TICKER_STYLE = {
+  'letterSpacing': '1px',  # Add spacing between the letters
+  'marginLeft': '10px',  # Add margin to the left
+  'width': '100px'  # Set a fixed width for the ticker
+}
+
+# Style settings for the stock price (stock information container)
+STOCK_PRICE_STYLE = {
+  'color': '#444444',  # Set the text color to dark gray
+  'marginLeft': '10px',  # Add margin to the left
+  'marginTop': '10px',  # Add margin to the top
+  'width': '100px',  # Set a fixed width for the price display
+  'text-align': 'right',  # Align the text to the right
+  'display': 'flex',  # Use flexbox for alignment
+  'justifyContent': 'flex-end'  # Align items to the end of the flex container
+}
+
+# Style settings for the currency (USD) (stock information container)
+CURRENCY_STYLE = {
+  'color': '#444444',  # Set the text color to dark gray
+  'fontSize': '20px',  # Set the font size to 20 pixels
+  'marginTop': '20px',  # Add margin to the top
+  'marginLeft': '1px',  # Add a small margin to the left
+  'margin-right': '0px',  # Remove margin on the right
+  'padding-left': '8px',  # Add padding to the left
+  'fontWeight': 'bold',  # Make the text bold
+  'text-align': 'right',  # Align the text to the right
+  'display': 'flex'  # Use flexbox for alignment
+}
+
+# Style settings for the stock indicator container (stock information container)
+INDICATOR_CONTAINER_STYLE = {
+  'backgroundColor': '#F5F5F5',  # Set a light gray background color
+  'borderRadius': '20px',  # Make the corners of the container rounded
+  'padding': '20px',  # Add padding inside the container
+  'marginTop': '10px'  # Add margin to the top
+}
+
+# Style settings for the indicator term text (e.g., 'P/E') (stock information container)
+INDICATOR_TERM_STYLE = {
+  'color': '#444444',  # Set the text color to dark gray
+  'fontWeight': 'bold'  # Make the text bold
+}
+
+# Style settings for the stock indicator values (stock information container)
+INDICATOR_STYLE = {
+  'color': '#444444',  # Set the text color to dark gray
+}
+
+# Style settings for the Google Trends container
+GTREND_CONTAINER_STYLE = {
+  'backgroundColor': '#F5F5F5',  # Set a light gray background color
+  'borderRadius': '20px',  # Make the corners of the container rounded
+  'padding': '20px',  # Add padding inside the container
+  'paddingBottom': '20px',  # Add padding specifically to the bottom
+  'width': '100%',  # Make the container take up the full width of its parent
+}
+
+# Style settings for the prediction plot container
+PREDICTION_PLOT_CONTAINER_STYLE = {
+  'maxWidth': '120%',  # Set a maximum width of 120% of the parent container
+  'backgroundColor': '#F5F5F5',  # Set a light gray background color
+  'marginBottom': '20px',  # Add margin to the bottom
+  'borderRadius': '20px',  # Make the corners of the container rounded
+  'padding': '20px',  # Add padding inside the container
+  'marginTop': '20px'  # Add margin to the top
+}
+
+# Style settings for the news container
 NEWS_CONTAINER_STYLE = {
-  'backgroundColor': '#F5F5F5',
-  'borderRadius': '20px',
-  'padding': '20px',
-  'marginBottom': '20px',
-  'marginTop': '0px',
-  'maxWidth': '120%'
+  'backgroundColor': '#F5F5F5',  # Set a light gray background color
+  'borderRadius': '20px',  # Make the corners of the container rounded
+  'padding': '20px',  # Add padding inside the container
+  'marginBottom': '20px',  # Add margin to the bottom
+  'marginTop': '0px',  # No margin at the top
+  'maxWidth': '120%'  # Set a maximum width of 120% of the parent container
 }
